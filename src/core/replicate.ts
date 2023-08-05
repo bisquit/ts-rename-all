1de41e81d531e999e2a10493c30dbf150b9fc2cf
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-08-05 15:43:02 +0900

    chore: package

--- src/core/replicate.ts
+++ src/core/replicate.ts
@@ -1,9 +1,3 @@
-/**
- * ```sh
- * tsrep fixtures/AppButton.tsx
- * ```
- */
-
 import { resolve } from 'node:path';
 
 import { Project } from 'ts-morph';
@@ -11,6 +5,10 @@ import { Project } from 'ts-morph';
 import { copy } from '../utils/copy.js';
 import { renameSymbols } from './renameSymbols.js';
 
+/**
+ * replicate a file
+ * - renaming symbols
+ */
 export async function replicate(
   srcFilePath: string,
   config: { destFileName: string; srcSymbol: string; destSymbol: string },
@@ -23,14 +21,7 @@ export async function replicate(
   const project = new Project({});
   const sourceFile = project.addSourceFileAtPath(destFilePath);
 
-  // await renameFilename(sourceFile, { srcSymbol, destSymbol });
   await renameSymbols(sourceFile, { srcSymbol, destSymbol });
 
   await project.save();
 }
-
-// await replicate(resolve('fixtures/AppButton.tsx'), {
-//   destFileName: 'AppCard.tsx',
-//   srcSymbol: 'AppButton',
-//   destSymbol: 'AppCard',
-// });
--- src/core/replicateDir.ts
+++ src/core/replicateDir.ts
@@ -1,9 +1,3 @@
-/**
- * ```sh
- * tsrep fixtures/AppButton.tsx
- * ```
- */
-
 import { existsSync } from 'node:fs';
 import { rm } from 'node:fs/promises';
 import { resolve } from 'node:path';
@@ -15,22 +9,24 @@ import { debug } from '../utils/debug.js';
 import { renameFilename } from './renameFilename.js';
 import { renameSymbols } from './renameSymbols.js';
 
+/**
+ * replicate a directory
+ * - renaming directory name
+ * - renaming symbols
+ */
 export async function replicateDir(
   srcDirPath: string,
   config: { destDirName: string; srcSymbol: string; destSymbol: string },
 ) {
   const { destDirName, srcSymbol, destSymbol } = config;
 
-  // copy directory
   const destDirPath = resolve(srcDirPath, '..', destDirName);
   if (existsSync(destDirPath)) {
     debug('removing', destDirPath);
     await rm(destDirPath, { recursive: true, force: true });
-    // throw new Error(`Directory already exists: ${destDirPath}`);
   }
   await copy(srcDirPath, destDirPath);
 
-  // project
   const project = new Project({});
   project.addSourceFilesAtPaths(`${destDirPath}/**/*.{ts,tsx}`);
 
@@ -41,9 +37,3 @@ export async function replicateDir(
 
   await project.save();
 }
-
-// await replicateDir(resolve('fixtures/app-button'), {
-//   destDirName: 'app-card',
-//   srcSymbol: 'AppButton',
-//   destSymbol: 'AppCard',
-// });
