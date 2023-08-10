99478dca0a716e30b5a563828cad613993f7aa51
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-08-11 07:43:24 +0900

    add changelogit

---
+++ changelogithub.config.cjs
@@ -0,0 +1,19 @@
+/*eslint-env commonjs*/
+
+/** @type {import('changelogithub').ChangelogOptions} */
+module.exports = {
+  types: {
+    feat: {
+      title: 'Features',
+    },
+    fix: {
+      title: 'Bug Fixes',
+    },
+    improve: {
+      title: 'Improvements',
+    },
+  },
+  titles: {
+    breakingChanges: 'Breaking changes',
+  },
+};
