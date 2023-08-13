ea058cb132880ac89376f403f0fe9d72ed747352
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-08-13 16:46:57 +0900

    modify pattern

--- packages/core/test/renameSymbols.test.ts
+++ packages/core/test/renameSymbols.test.ts
@@ -24,15 +24,33 @@ describe('renameSymbols', () => {
 
     const content = await readFile(resolveFixturePath('Foo.ts'), 'utf-8');
 
+    // check Foo renamed to Bar, and Hoo not renamed
+
     expect(content).toMatch('class Bar {');
+    expect(content).toMatch('class Hoo {');
+
     expect(content).toMatch('enum BarEnum {');
+    expect(content).toMatch('enum HooEnum {');
+
     expect(content).toMatch('function bar(');
+    expect(content).toMatch('function hoo(');
+
     expect(content).toMatch('interface BarInterface {');
+    expect(content).toMatch('interface HooInterface {');
+
     expect(content).toMatch('module BarModule {');
+    expect(content).toMatch('module HooModule {');
     expect(content).toMatch('namespace BarNamespace {');
+    expect(content).toMatch('namespace HooNamespace {');
+
     expect(content).toMatch('type BarType =');
+    expect(content).toMatch('type HooType =');
+
     expect(content).toMatch('const barConst =');
+    expect(content).toMatch('const hooConst =');
     expect(content).toMatch('let barLet =');
+    expect(content).toMatch('let hooLet =');
     expect(content).toMatch('var barVar =');
+    expect(content).toMatch('var hooVar =');
   });
 });
